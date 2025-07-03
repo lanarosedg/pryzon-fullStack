33e48517ca185a17830ab3e37198671333eb1f10
Lana Rose <lanarosedeguzman@gmail.com>
2025-07-03 08:23:09 +0800

    Addeed answer checking for L3

--- frontend/src/LogicLevels/L3.jsx
+++ frontend/src/LogicLevels/L3.jsx
@@ -8,21 +8,30 @@ import l3a from '../assets/LogicLevels/l3a.png';
 import l3b from '../assets/LogicLevels/l3b.png';
 import l3c from '../assets/LogicLevels/l3c.png';
 
+import next from '../assets/next.png';
+import { useNavigate } from 'react-router-dom';
 
 function L3() {
-    const [result , setResult] = useState('');
+    const [isCorrect, setIsCorrect] = useState(null);
+    const [shakeInput, setShakeInput] = useState(false);
+    const navigate = useNavigate();
 
     const handleClick = async (choice) => {
         try {
-            const response = await axios.post('http://localhost:8080/api/check-answer', {
+            const res = await axios.post('http://localhost:8080/api/check-answer', {
                 answer: choice,
                 level: "L3"
             });
-            setResult(response.data ? "Correct!" : "Wrong answer. Try again");
-        } catch (error) {
-            setResult("Error checking answer.");
-            console.error(error);
-        }
+            if (res.data === true) {
+                setIsCorrect(true);
+            } else {
+                setIsCorrect(false);
+                setShakeInput(true);
+                setTimeout(() => setShakeInput(false), 500); 
+            }
+            }  catch (err) {
+                console.error("Error checking answer", err);
+            }
     };
 
     return (
@@ -42,6 +51,7 @@ function L3() {
                     className="l3ProblemImg" 
                 />
             </div>
+            <div className={`choicesContainer ${shakeInput ? 'shake' : ''}`}>
                 <img 
                     src={l3a} 
                     alt="" 
@@ -60,9 +70,18 @@ function L3() {
                     className="l3c" 
                     onClick={() => handleClick("C")}
                 />
-                <div className="resultMessage">
-                    {result}
                 </div>
+                {isCorrect === true && (
+                    <div className="correct-animationL1">
+                        <p>✔ Correct!</p>
+                        <img
+                            src={next}
+                            alt="Next"
+                            className="nextButton"
+                            onClick={() => navigate('/LogicLevels/L2')}
+                        />
+                    </div>
+                )}
         </div>
         </>
     )
